efaa2a149d74b5bc007eee02e15be00b4d754cb4
brice <bristimo16@gmail.com>
2021-08-28 03:59:43 +0000

    second commit

---
+++ deploy.java
@@ -0,0 +1,6 @@
+#https://github.com/Landmark-Technologies
+# https://github.com/orgs/Landmark-Technologies/teams/paypaldevelopers
+# https://github.com/orgs/Landmark-Technologies/teams/devopslead25
+# https://github.com/Landmark-Technologies/paypal-
+# https://github.com/Landmark-Technologies/paypal-frontend
+
